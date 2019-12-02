52fb17b9eb9f727bc20a2a6621424af80cd16d8b
jepusto <jepusto@gmail.com>
2019-12-02 16:30:41 -0600

    Added some test models.

---
+++ tests/testthat/test-two-level.R
@@ -0,0 +1,38 @@
+library(nlme)
+data(Laski)
+
+Laski_iid <- lme(fixed = outcome ~ treatment,
+                 random = ~ treatment | case,
+                 data = Laski)
+
+Laski_het <- lme(fixed = outcome ~ treatment,
+                 random = ~ treatment | case,
+                 weights = varIdent(form = ~ 1 | treatment),
+                 data = Laski)
+
+Laski_AR1 <- lme(fixed = outcome ~ treatment,
+                 random = ~ treatment | case,
+                 correlation = corAR1(0, ~ time | case),
+                 data = Laski)
+
+Laski_hetAR1 <- lme(fixed = outcome ~ treatment,
+                    random = ~ treatment | case,
+                    correlation = corAR1(0, ~ time | case),
+                    weights = varIdent(form = ~ 1 | treatment),
+                    data = Laski)
+
+Laski_CAR1 <- lme(fixed = outcome ~ treatment,
+                  random = ~ treatment | case,
+                  correlation = corCAR1(0.2, ~ time | case),
+                  data = Laski)
+
+Laski_MA1 <- lme(fixed = outcome ~ treatment,
+                 random = ~ treatment | case,
+                 correlation = corARMA(0, ~ time | case, p = 0, q = 1),
+                 data = Laski)
+
+
+extract_varcomp(Laski_iid)
+
+mod <- Laski_iid
+getGroups(mod)
