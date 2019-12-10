4c9660a63174d033d6d1054e3424d7c6898b94be
jepusto <jepusto@gmail.com>
2019-12-10 14:57:06 -0600

    Added documentation for Laski data.

---
+++ R/data-documentation.R
@@ -0,0 +1,23 @@
+#' Laski, Charlop, & Schreibman (1988)
+#'
+#' Data from a multiple baseline design conducted by
+#' Laski, Charlop, & Schreibman (1988). The variables are as follows:
+#' \itemize{
+#'   \item \code{case}. Child identifier.
+#'   \item \code{outcome}. Frequency of child vocalization, as measured by
+#'   a partial interval recording procedure with 60 ten-second intervals per session.
+#'   \item \code{time}. Measurement occasion.
+#'   \item \code{treatment}. Indicator for treatment phase.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 128 rows and 4 variables
+#' @name Laski
+#' @source Laski, K. E., Charlop, M. H., & Schreibman, L. (1988).
+#' Training parents to use the natural language paradigm to increase
+#' their autistic children's speech.
+#' \emph{Journal of Applied Behavior Analysis, 21}(4), 391-400.
+#'
+
+NULL
