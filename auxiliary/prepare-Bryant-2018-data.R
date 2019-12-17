46a9f748a091b7851202a4e85315892f8762adb3
Man Chen <amandachen@utexas.edu>
2019-12-17 03:46:09 -0600

    Updated Bryant 2018 data.

--- auxiliary/prepare-Bryant-2018-data.R
+++ auxiliary/prepare-Bryant-2018-data.R
@@ -1,10 +1,10 @@
 # Read in Bryant 2018
 
-Bryant18 <- read.csv("auxiliary/Bryant2018.csv", stringsAsFactors = FALSE)
-str(Bryant18)
+Bryant2018 <- read.csv("auxiliary/Bryant2018.csv", stringsAsFactors = FALSE)
+str(Bryant2018)
 
-Bryant18 <- within(Bryant18, {
-  Study_ID <- rep("Bryant2018", length(Bryant18$school))
+Bryant2018 <- within(Bryant2018, {
+  Study_ID <- rep("Bryant2018", length(Bryant2018$school))
   school <- factor(paste("school", school))
   case <- factor(studentID)
   outcome <- AC
@@ -12,6 +12,14 @@ Bryant18 <- within(Bryant18, {
   treatment <- factor(treatment)
 })
 
-Bryant18 <- Bryant18[c("Study_ID", "school","case","treatment","session","session_trt","outcome")]
+Bryant2018 <- Bryant2018[c("Study_ID", "school","case","treatment","session","session_trt","outcome")]
 
-save(Bryant18, file = "data/Bryant2018.RData", compress = TRUE)
+# time-point constants
+A <- 4
+B <- 21
+
+# center at follow-up time
+Center <- B
+Bryant2018$session_c <-Bryant2018$session - Center
+
+save(Bryant2018, file = "data/Bryant2018.RData", compress = TRUE)
