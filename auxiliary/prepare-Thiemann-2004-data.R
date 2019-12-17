f56a1b3a8ad8c2feac71f9b03b33a259b56d32fe
Man Chen <amandachen@utexas.edu>
2019-12-17 03:27:37 -0600

    Updated Thiemann 2014 data.

--- auxiliary/prepare-Thiemann-2004-data.R
+++ auxiliary/prepare-Thiemann-2004-data.R
@@ -1,13 +1,27 @@
-# Read in Thiemann 2001
+# Read in Thiemann 2004
 
-Thiemann2001 <- read.csv("auxiliary/Thiemann2001.csv", stringsAsFactors = FALSE)
-str(Thiemann2001)
+Thiemann2004 <- read.csv("auxiliary/Thiemann2004.csv", stringsAsFactors = FALSE)
+str(Thiemann2004)
 
-Thiemann2001 <- within(Thiemann2001, {
+Thiemann2004 <- within(Thiemann2004, {
   case <- factor(case)
   series <- factor(series)
   treatment <- ifelse(treatment == "A", "baseline", "treatment")
   treatment <- factor(treatment)
 })
 
-save(Thiemann2001, file = "data/Thiemann2001.RData", compress = TRUE)
+Thiemann2004 <- Thiemann2004 %>%
+  arrange(case, series, time) %>%
+  group_by(case, series) %>%
+  mutate(
+    trt_time = pmax(0, time - max(time[treatment == "A"])))
+
+# time-point constants
+A <- 10
+B <- 33
+
+# center at follow-up time
+Center <- B
+Thiemann2004$time_c <- Thiemann2004$time - Center
+
+save(Thiemann2004, file = "data/Thiemann2004.RData", compress = TRUE)
