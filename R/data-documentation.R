8f9ae0a941016749f1d2bca72a584b06e0d55c48
Man Chen <amandachen@utexas.edu>
2019-12-17 02:22:35 -0600

    Added documentation for Thiemann (2001 and 2004), Bryant (2016 and 2018).

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -21,3 +21,115 @@
 #'
 
 NULL
+
+#' Thiemann & Goldstein (2001)
+#'
+#' Data from a multiple baseline across behaviors design conducted by
+#' Thiemann & Goldstein (2001). The variables are as follows:
+#' \itemize{
+#'   \item \code{Study_ID}. Study identifier.
+#'   \item \code{case}. Student identifier.
+#'   \item \code{series}. Series identifier.
+#'   \item \code{outcome}. Frequency of coded social communication skills, as measured by
+#'   a direct observation coding system with 15-second intervals recoding for the occurrence
+#'   of any of the four social measures: contingent responses, securing attention, initiating
+#'   comments, and initiating requests.
+#'   \item \code{time}. Measurement occasion.
+#'   \item \code{treatment}. Indicator for treatment phase.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 221 rows and 6 variables
+#' @name Thiemann2001
+#' @source Thiemann, K.S., & Goldstein, H. (2001).
+#' Social stories, written text cues, and video feedback: effects on social communication
+#' of children with Autism.
+#' \emph{Journal of Applied Behavior Analysis, 34}(4), 425-446.
+#'
+
+NULL
+
+#' Thiemann & Goldstein (2004)
+#'
+#' Data from a multiple baseline across behaviors design conducted by
+#' Thiemann & Goldstein (2004). The variables are as follows:
+#' \itemize{
+#'   \item \code{Study_ID}. Study identifier.
+#'   \item \code{case}. Student identifier.
+#'   \item \code{series}. Series identifier.
+#'   \item \code{outcome}. Frequency of unprompted targeted social communication skills,
+#'   as measured by a direct observation, paper and pencil coding system during the 10-minute
+#'   social activity for each behavior for all sessions.
+#'   \item \code{time}. Measurement occasion.
+#'   \item \code{treatment}. Indicator for treatment phase.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 408 rows and 6 variables
+#' @name Thiemann2004
+#' @source Thiemann, K.S., & Goldstein, H. (2004).
+#' Effects of peer training and written text cueing on social communication of
+#' school-age children with pervasive developmental disorder.
+#' \emph{Journal of Speech Language and Hearing Research, 47}(1), 126-144.
+#'
+
+NULL
+
+#' Bryant et al. (2016)
+#'
+#' Data from a multiple baseline across clusters design conducted by
+#' Bryant et al. (2016). The variables are as follows:
+#' \itemize{
+#'   \item \code{Study_ID}. Study identifier.
+#'   \item \code{school}. School identifier.
+#'   \item \code{case}. Student identifier.
+#'   \item \code{session}. Measurement occasion.
+#'   \item \code{treatment}. Indicator for treatment phase.
+#'   \item \code{outcome}. Texas Early Mathematics Inventory (TEMI-Aim Check) scores.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 299 rows and 6 variables
+#' @name Bryant2016
+#' @source Bryant, B. R., Bryant, D. P., Porterfield, J., Dennis, M. S., Falcomata, T.,
+#' Valentine, C., Brewer, C., & Bell, K. (2016).
+#' The effects of a Tier 3 intervention on the mathematics performance of second grade
+#' students with severe mathematics difficulties.
+#' \emph{Journal of Learning Disabilities, 49}(2), 176-188.
+#'
+
+NULL
+
+#' Bryant et al. (2018)
+#'
+#' Data from a multiple baseline across clusters design conducted by
+#' Bryant et al. (2018). The variables are as follows:
+#' \itemize{
+#'   \item \code{school}. School identifier.
+#'   \item \code{group}. Group identifier.
+#'   \item \code{key_math_std_score}. Key Math Standard Score (Age).
+#'   \item \code{student}. Student identifier.
+#'   \item \code{S}. Session identifier.
+#'   \item \code{AC}. Outcome.
+#'   \item \code{Measure}. Outcome measurement.
+#'   \item \code{phase}. Indicator for treatment phase.
+#'   \item \code{session}. Measurement occasion.
+#'   \item \code{session_trt}. Measurement occasion times treatment phase.
+#'   \item \code{studentID}. Categorical indicator for student.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 536 rows and 11 variables
+#' @name Bryant2018
+#' @source Bryant, D. R., Bryant, B. R., Sorelle-Miner, D. A., Falcomata, T. S.
+#' & Nozari, M. (2018).
+#' Tier 3 intensified intervention for second grade students with severe
+#' mathematics difficulties.
+#' \emph{Archives of Psychology, 2}(11), 1-24.
+#'
+
+NULL
