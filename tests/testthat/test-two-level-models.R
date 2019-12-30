013cf32fb511917eb7c32c73eeb9b32732cf7c4a
James Pustejovsky <jepusto@gmail.com>
2019-12-30 09:58:30 -0600

    Modified unit tests for targetVariance() to check that the function returns a list of matrices with correct dimensions. Closes #3.

--- tests/testthat/test-two-level-models.R
+++ tests/testthat/test-two-level-models.R
@@ -36,23 +36,20 @@ extract_varcomp(Laski_iid)
 
 mod <- Laski_iid
 getGroups(mod)
+grp <- Laski$case
 
-targetVariance(mod)
-
-test_that("targetVariance() works with 2-level models.", {
-  expect_output(targetVariance(Laski_iid))
-  expect_output(targetVariance(Laski_het))
-  expect_output(targetVariance(Laski_AR1))
-  expect_output(targetVariance(Laski_hetAR1))
-  expect_output(targetVariance(Laski_CAR1))
-  expect_output(targetVariance(Laski_MA1))
-})
+expect_correct_dims <- function(mod, grp) {
+  grp_size <- as.numeric(table(grp))
+  dims <- sapply(targetVariance(mod), dim)
+  expect_equal(grp_size, dims[1,], check.attributes = FALSE)
+  expect_equal(grp_size, dims[2,], check.attributes = FALSE)
+}
 
 test_that("targetVariance() works with 2-level models.", {
-  expect_is(targetVariance(Laski_iid), "list")
-  expect_is(targetVariance(Laski_het), "list")
-  expect_is(targetVariance(Laski_AR1), "list")
-  expect_is(targetVariance(Laski_hetAR1), "list")
-  expect_is(targetVariance(Laski_CAR1), "list")
-  expect_is(targetVariance(Laski_MA1), "list")
+  expect_correct_dims(Laski_iid, Laski$case)
+  expect_correct_dims(Laski_het, Laski$case)
+  expect_correct_dims(Laski_AR1, Laski$case)
+  expect_correct_dims(Laski_hetAR1, Laski$case)
+  expect_correct_dims(Laski_CAR1, Laski$case)
+  expect_correct_dims(Laski_MA1, Laski$case)
 })
