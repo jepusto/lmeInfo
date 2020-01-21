a1a58b36b3bb9e18287562f8b14366fcba37ec39
Man Chen <amandachen@utexas.edu>
2020-01-21 00:45:55 -0600

    Added multivariate multilevel model.

---
+++ tests/testthat/test-multivariate-models.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(tidyr)
+library(nlme)
+data(bdf, package = "mlmRev")
+
+bdf_long <-
+  bdf %>%
+  pivot_longer(cols = c(IQ.verb, IQ.perf, aritPRET),
+               names_to = "measure",
+               values_to = "score") %>%
+  select(schoolNR, pupilNR, sex, Minority, measure, score)
+
+bdf_MVML <- lme(score ~ 0 + measure,
+                random = ~ 1| schoolNR / pupilNR,
+                corr = corSymm(form = ~ 1 | schoolNR / pupilNR),
+                weights = varIdent(form = ~ 1 | measure),
+                data = bdf_long)
+
+summary(bdf_MVML)
+
+bdf_MVML$modelStruct$corStruct
+bdf_MVML$modelStruct$varStruct
